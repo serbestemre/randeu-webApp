8b75c97162be93555784233c279c749d383bdd56
Emre <srbstemre@gmail.com>
2020-05-25 18:53:45 +0300

    register page is added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Landing from "./pages/Landing/Landing";
 import Layout from "./hoc/Layout/Layout";
 import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 import Auxiliary from "./hoc/Auxiliary/Auxiliary";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
      <Landing /> 
     </Layout>
     <Login/>
+    <Register/>
    </Auxiliary>
   );
 };
