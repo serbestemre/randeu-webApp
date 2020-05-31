dde86ab2e44870d577d7969e7160b46ed12947df
Emre <srbstemre@gmail.com>
2020-05-31 18:37:32 +0300

    kullanıcı giriş is re-designed

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,31 +5,17 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
+import {Link} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Randeu
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
+    marginBottom: theme.spacing(8),    display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
   },
@@ -66,7 +52,7 @@ export default function Login() {
             required
             fullWidth
             id="email"
-            label="Email Address"
+            label="Email Adresiniz"
             name="email"
             autoComplete="email"
             autoFocus
@@ -77,14 +63,14 @@ export default function Login() {
             required
             fullWidth
             name="password"
-            label="Password"
+            label="Şifreniz"
             type="password"
             id="password"
             autoComplete="current-password"
           />
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
+            label="Beni Hatırla"
           />
           <Button
             type="submit"
@@ -93,25 +79,34 @@ export default function Login() {
             color="primary"
             className={classes.submit}
           >
-            Sign In
+            Giriş Yap
           </Button>
           <Grid container>
             <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+            <Typography
+                variant="body2"
+                component={Link}
+                color="secondary"
+                to="/kullanici/kayit"
+              >
+                Şifremi Unuttum
+              </Typography>
             </Grid>
             <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
+            <Typography
+                variant="body2"
+                component={Link}
+                color="secondary"
+                to="/kullanici/kayit"
+              >
+                Hesabınız yok mu? Hemen Kayıt ol
+              </Typography>
+        
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
+
     </Container>
   );
 }
